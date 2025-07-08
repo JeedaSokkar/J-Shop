5b7ae09905b76a6293671f2eaecf640028026e69
jeeda3 <jeedasokkar2003@gmail.com>
2025-07-08 22:59:39 +0300

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import AuthLayout from './layouts/AuthLayout';
 import Login from "./pages/user/login/Login"
 import Register from './pages/user/register/Register'
 
-import DashboardLayout from './layouts/dashboardLayout';
+import DashboardLayout from './layouts/DashboardLayout';
 import { ToastContainer} from 'react-toastify';
 import UserLayout from './layouts/UserLayout';
 import Home from './pages/user/home/Home';
